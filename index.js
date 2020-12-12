b53c5a3eeaaef8dd722926c012e60a911a02f39e
tbanish <tombanish@gmail.com>
2020-12-12 14:55:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(array, fn) {
+  let newArray = []
+
+  for (const num of array) {
+    newArray.push(fn(num))
+  }
+  return newArray
+}
+
+function reduce(array, fn, start) {
+  let memo = true
+
+  for(const num of array) {
+    memo = fn(num, memo)
+  }
+
+  if (typeof memo == 'number') {
+    memo -= 1
+  }
+
+  if (start == undefined) {
+    return memo
+  } else {
+    return memo + start
+  }
+}
